eb4297a904020a13e0250bdf2e5abda7b03364fe
Hidayat Nurwahid <hidayatnurwahid969@gmail.com>
2024-07-11 22:47:05 +0700

    change baseurl

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     // public string $baseURL = 'http://localhost:8080/askar';
-    public string $baseURL = 'http://localhost:8080/askar/';
+    public string $baseURL = 'http://localhost:8080/askar-project/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
