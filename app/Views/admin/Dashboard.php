d250b95a12d3bf0b9c5e91182cf89376b41b8c52
Hidayat Nurwahid <hidayatnurwahid969@gmail.com>
2024-07-18 20:43:01 +0700

    Dashboard Session Display Name

--- app/Views/admin/Dashboard.php
+++ app/Views/admin/Dashboard.php
@@ -108,7 +108,7 @@
                 </div>
                 <div class="sb-sidenav-footer">
                     <div class="small">Logged in as:</div>
-                    Start Bootstrap
+                    <?php $session = session(); echo $_SESSION['nama']; ?>
                 </div>
             </nav>
         </div>
